c52ae237a32257ab2e8fe46c4b2f38ed3836e47e
oosoriodr <113384970+oosoriodr@users.noreply.github.com>
2022-09-28 00:07:48 -0400

    navbar styles fixed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
