f6f36b6d392f1f93820df2ad934e162a00d4bfec
oosoriodr <113384970+oosoriodr@users.noreply.github.com>
2022-10-04 21:50:01 -0400

    Paths fixed & new pages added
    Contact list, update view.

--- routes/business.js
+++ routes/business.js
@@ -4,7 +4,19 @@ var router = express.Router();
 /* GET Business page. */
 router.get('/', function (req, res, next) {
   var contactList = [{ name: "jhon mcdonals", number: "2313334344", email: "jmc@gmail.com" }, { name: "sara coxwell", number: "34290923344", email: "sra.cw@amazon.com" }];
-  res.render('business', { title: 'Business', contactList: contactList });
+  res.render('business/business', { title: 'Business', contactList: contactList });
+});
+
+router.get('/update/:contactId', function (req, res, next) {
+  var contactDetails = { name: "jhon mcdonals", number: "2313334344", email: "jmc@gmail.com" };
+  console.log("contactId");
+  res.render('business/update', { title: 'Business Update', contactDetails: contactDetails });
+});
+
+router.get('/delete/:contactId', function (req, res, next) {
+  var contactDetails = { name: "jhon mcdonals", number: "2313334344", email: "jmc@gmail.com" };
+  console.log("contactId");
+  res.render('business/update', { title: 'Business Update', contactDetails: contactDetails });
 });
 
 module.exports = router;
