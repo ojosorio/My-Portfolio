73865747fc8c61f4ccdd79d10a09ea313175dd17
oosoriodr <113384970+oosoriodr@users.noreply.github.com>
2022-10-27 14:55:44 -0400

    Mongo connection & website configuration added

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    URI: "mongodb://127.0.0.1:27000/portfolio",
+    //URI: "mongodb://127.0.0.1:27000/portfolio",
+    URI: "mongodb+srv://portfoliouser:gYkAZM0UqTtJR@cluster-oj.npogd6g.mongodb.net/portfolio?retryWrites=true&w=majority",
 };
